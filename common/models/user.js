3105924f1744ce20f78324aeca1341a7549f4e87
dan <danelebash@hotmail.com>
2017-08-15 22:56:16 -0400

    related Role model working by using attached method

--- common/models/user.js
+++ common/models/user.js
@@ -1,14 +1,18 @@
 let jwt = require('jsonwebtoken');
 let config = require('../../server/config.json');
-let models = require('../../server/model-config.json');
 let app = require('../../server/server.js');
-let Role = app.models.role;
-let RoleMapping = app.models.rolemapping;
+let _ = require('lodash')
 
 
 module.exports = function (user) {
+  let Role;
+  let RoleMapping;
 
-  // // provide own createAccessToken to create JWTs
+  user.on('attached', function (a) {
+    Role = app.models.Role;
+    RoleMapping = app.models.RoleMapping;
+    // perform any setup that requires the app object
+  });
   user.prototype.createAccessToken = function (ttl, options, cb) {
     if (cb === undefined && typeof options === 'function') {
       cb = options;
@@ -42,72 +46,12 @@ module.exports = function (user) {
     if (typeof cb !== 'function') return response;
 
     return cb(null, response);
-  }
-
-  // Add method addRole to user
-  user.addRole = function (id, role, next) {
-    next = next || function () {
-    };
-    return Role.findOne({where: {name: role}})
-      .then(function (role) {
-        if (!role) return sendError(next, 'No role found', 404);
-        return RoleMapping.findOrCreate({principalType: RoleMapping.USER, principalId: id, roleId: role.id});
-      })
-      .then(function (data) {
-        next(null);
-        return null;
-      })
-      .catch(function (error) {
-        return sendError(next, 'Internal error', 500);
-      })
   };
 
-  // Add method removeRole to user
-  user.removeRole = function (id, role, next) {
-    next = next || function () {
-    };
-    return Role.findOne({where: {name: role}})
-      .then(function (role) {
-        if (!role) return sendError(next, 'No role found', 404);
-        return RoleMapping.findOne({where: {principalType: RoleMapping.USER, principalId: id, roleId: role.id}});
-      })
-      .then(function (roleMapping) {
-        if (!roleMapping) return;
-        return roleMapping.destroy();
-      })
-      .then(function () {
-        next(null);
-        return null;
-      })
-      .catch(function (error) {
-        return sendError(next, 'Internal error', 500);
-      })
-  };
-
-  // Add method findByRole to user
-  user.findByRole = function (role, next) {
-    next = next || function () {
-    };
-    return Role.findOne({where: {name: role}})
-      .then(function (role) {
-        if (!role) return sendError(next, 'No role found', 404);
-        return RoleMapping.find({where: {roleId: role.id, principalType: 'USER'}});
-      })
-      .then(function (roleMappings) {
-        let ids = _.uniq(_.map(roleMappings, 'principalId'));
-        return user.find({where: {id: {inq: ids}}});
-      })
-      .then(function (users) {
-        next(null, users);
-        return users;
-      })
-      .catch(function (error) {
-        return sendError(next, 'Internal error', 500);
-      })
-  };
 
   // Add method getAllRoles to user
   user.getAllRoles = function (req, next) {
+
     let userId = req.accessToken.userId;
     if (!userId) {
       return sendError(next, 'You are not connected', 401);
@@ -140,84 +84,6 @@ module.exports = function (user) {
     });
   };
 
-  // Add method getPersistedRoles to user
-  user.getPersistedRoles = function (id, next) {
-    next = next || function () {
-    };
-    return RoleMapping.find({where: {principalType: 'USER', principalId: id}})
-      .then(function (roleMappings) {
-        let ids = _.uniq(_.map(roleMappings, 'roleId'));
-        if (ids.length > 0) {
-          return Role.find({where: {id: {inq: ids}}});
-        } else {
-          return [];
-        }
-      })
-      .then(function (roles) {
-        let result = _.map(roles, 'name');
-        next(null, result);
-        return result;
-      })
-      .catch(function (error) {
-        return sendError(next, 'Internal error', 500);
-      })
-  };
-
-  // Register remote method
-  user.remoteMethod('addRole', {
-    accepts: [
-      {
-        arg: 'id',
-        type: 'string',
-        required: true
-      },
-      {
-        arg: 'role',
-        type: 'string',
-        required: true
-      }
-    ],
-    returns: {}
-    ,
-    http: {
-      verb: 'post',
-      path: '/:id/addRole'
-    }
-  });
-  user.remoteMethod('removeRole', {
-    accepts: [
-      {
-        arg: 'id',
-        type: 'string',
-        required: true
-      },
-      {
-        arg: 'role',
-        type: 'string',
-        required: true
-      }
-    ],
-    returns: {}
-    ,
-    http: {
-      verb: 'post',
-      path: '/:id/removeRole'
-    }
-  });
-  user.remoteMethod('findByRole', {
-    accepts: {
-      arg: 'role',
-      type: 'string',
-      required: true
-    },
-    returns: {
-      arg: 'users',
-      type: '[object]'
-    },
-    http: {
-      verb: 'get'
-    }
-  });
   user.remoteMethod('getAllRoles', {
     accepts: [
       {
@@ -234,25 +100,179 @@ module.exports = function (user) {
     ,
     http: {
       verb: 'get',
-      path: '/roles'
+      path: '/myrole'
     }
   });
-  user.remoteMethod('getPersistedRoles', {
-    accepts: [
-      {
-        arg: 'id',
-        type: 'string',
-        required: true
-      }
-    ],
-    returns: {
-      arg: 'roles',
-      type: '[string]'
-    }
-    ,
-    http: {
-      verb: 'get',
-      path: '/:id/persistedRoles'
-    }
-  });
+
+
+  //
+  // // Add method addRole to user
+  // user.addRole = function (id, role, next) {
+  //   next = next || function () {
+  //   };
+  //   return Role.findOne({where: {name: role}})
+  //     .then(function (role) {
+  //       if (!role) return sendError(next, 'No role found', 404);
+  //       return RoleMapping.findOrCreate({principalType: RoleMapping.USER, principalId: id, roleId: role.id});
+  //     })
+  //     .then(function (data) {
+  //       next(null);
+  //       return null;
+  //     })
+  //     .catch(function (error) {
+  //       return sendError(next, 'Internal error', 500);
+  //     })
+  // };
+  //
+
+
+  // // Add method removeRole to user
+  // user.removeRole = function (id, role, next) {
+  //   next = next || function () {
+  //   };
+  //   return Role.findOne({where: {name: role}})
+  //     .then(function (role) {
+  //       if (!role) return sendError(next, 'No role found', 404);
+  //       return RoleMapping.findOne({where: {principalType: RoleMapping.USER, principalId: id, roleId: role.id}});
+  //     })
+  //     .then(function (roleMapping) {
+  //       if (!roleMapping) return;
+  //       return roleMapping.destroy();
+  //     })
+  //     .then(function () {
+  //       next(null);
+  //       return null;
+  //     })
+  //     .catch(function (error) {
+  //       return sendError(next, 'Internal error', 500);
+  //     })
+  // };
+  //
+
+
+  // // Add method findByRole to user
+  // user.findByRole = function (role, next) {
+  //   next = next || function () {
+  //   };
+  //   return Role.findOne({where: {name: role}})
+  //     .then(function (role) {
+  //       if (!role) return sendError(next, 'No role found', 404);
+  //       return RoleMapping.find({where: {roleId: role.id, principalType: 'USER'}});
+  //     })
+  //     .then(function (roleMappings) {
+  //       let ids = _.uniq(_.map(roleMappings, 'principalId'));
+  //       return user.find({where: {id: {inq: ids}}});
+  //     })
+  //     .then(function (users) {
+  //       next(null, users);
+  //       return users;
+  //     })
+  //     .catch(function (error) {
+  //       return sendError(next, 'Internal error', 500);
+  //     })
+  // };
+  //
+  //
+  //
+
+
+  // // Add method getPersistedRoles to user
+  // user.getPersistedRoles = function (id, next) {
+  //   next = next || function () {
+  //   };
+  //   return RoleMapping.find({where: {principalType: 'USER', principalId: id}})
+  //     .then(function (roleMappings) {
+  //       let ids = _.uniq(_.map(roleMappings, 'roleId'));
+  //       if (ids.length > 0) {
+  //         return Role.find({where: {id: {inq: ids}}});
+  //       } else {
+  //         return [];
+  //       }
+  //     })
+  //     .then(function (roles) {
+  //       let result = _.map(roles, 'name');
+  //       next(null, result);
+  //       return result;
+  //     })
+  //     .catch(function (error) {
+  //       return sendError(next, 'Internal error', 500);
+  //     })
+  // };
+  //
+
+
+  // // Register remote method
+  // user.remoteMethod('addRole', {
+  //   accepts: [
+  //     {
+  //       arg: 'id',
+  //       type: 'string',
+  //       required: true
+  //     },
+  //     {
+  //       arg: 'role',
+  //       type: 'string',
+  //       required: true
+  //     }
+  //   ],
+  //   returns: {}
+  //   ,
+  //   http: {
+  //     verb: 'post',
+  //     path: '/:id/addRole'
+  //   }
+  // });
+  // user.remoteMethod('removeRole', {
+  //   accepts: [
+  //     {
+  //       arg: 'id',
+  //       type: 'string',
+  //       required: true
+  //     },
+  //     {
+  //       arg: 'role',
+  //       type: 'string',
+  //       required: true
+  //     }
+  //   ],
+  //   returns: {}
+  //   ,
+  //   http: {
+  //     verb: 'post',
+  //     path: '/:id/removeRole'
+  //   }
+  // });
+  // user.remoteMethod('findByRole', {
+  //   accepts: {
+  //     arg: 'role',
+  //     type: 'string',
+  //     required: true
+  //   },
+  //   returns: {
+  //     arg: 'users',
+  //     type: '[object]'
+  //   },
+  //   http: {
+  //     verb: 'get'
+  //   }
+  // });
+  //
+  // user.remoteMethod('getPersistedRoles', {
+  //   accepts: [
+  //     {
+  //       arg: 'id',
+  //       type: 'string',
+  //       required: true
+  //     }
+  //   ],
+  //   returns: {
+  //     arg: 'roles',
+  //     type: '[string]'
+  //   }
+  //   ,
+  //   http: {
+  //     verb: 'get',
+  //     path: '/:id/persistedRoles'
+  //   }
+  // });
 };
